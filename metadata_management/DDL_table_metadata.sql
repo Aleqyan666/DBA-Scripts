3f0a7136b5f1432445669b461121adf4180c2b54
halekyan <haykalekyan@gmail.com>
2026-05-27 17:05:57 +0400

    fixed mistake of redundant page calculation (bigger size) sp_GetAssetInfoTables.sql

--- metadata_management/DDL_table_metadata.sql
+++ metadata_management/DDL_table_metadata.sql
@@ -8,20 +8,20 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[table_metadata](
- [id] [bigint] IDENTITY(1,1) NOT NULL,
- [table_name] [sysname] NOT NULL,
- [database_name] [sysname] NOT NULL,
- [schema_name] [sysname] NOT NULL,
- [row_count] [bigint] NULL,
- [column_count] [smallint] NULL,
- [indexes] [tinyint] NULL,
- [table_size_mb] [bigint] NULL,
- [index_size_mb] [bigint] NULL,
- [created_date] [datetime] NULL,
- [last_modified_date] [datetime] NULL,
- [last_date_update] [datetime] NULL,
- [inserted_at] [datetime] NULL,
- CONSTRAINT [PK_table_metadata] PRIMARY KEY CLUSTERED 
+    [id] [bigint] IDENTITY(1,1) NOT NULL,
+    [table_name] [sysname] NOT NULL,
+    [database_name] [sysname] NOT NULL,
+    [schema_name] [sysname] NOT NULL,
+    [row_count] [bigint] NULL,
+    [column_count] [smallint] NULL,
+    [indexes] [tinyint] NULL,
+    [table_size_mb] [bigint] NULL,
+    [index_size_mb] [bigint] NULL,
+    [created_date] [datetime] NULL,
+    [last_modified_date] [datetime] NULL,
+    [last_date_update] [datetime] NULL,
+    [inserted_at] [datetime] NULL,
+    CONSTRAINT [PK_table_metadata] PRIMARY KEY CLUSTERED 
 (
  [id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 95) ON [PRIMARY]
